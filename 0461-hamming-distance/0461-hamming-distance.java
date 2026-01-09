7fa841f49c789657a8e92d7c36f46be2fedf78cd
Kundan Kumar Sharma <146640526+kundan9199@users.noreply.github.com>
2026-01-09 23:24:31 +0530

    Time: 0 ms (100%), Space: 42.3 MB (40.58%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int cnt = 0;
+        int n = x ^ y;
+        while(n != 0) {
+            n = n & (n-1);
+            cnt++;
+        }
+        return cnt;
+    }
+} 
