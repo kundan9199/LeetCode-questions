cb1eae9910cd47b4cc1d2d7456d0f5f82e686383
Kundan Kumar Sharma <146640526+kundan9199@users.noreply.github.com>
2026-04-20 00:37:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (13.64%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        int l=1;
-       int r=x;
-       while(l<=r){
-           int m=(r-l)/2+l;
-           if(m>x/m) r=m-1;
-           else if(m<x/m) l=m+1;
-           else return m;
-       }
-       return r;
+        int l = 1;
+        int r = x;
+        while (l <= r) {
+            int m = (r - l) / 2 + l;
+            if (m > x / m) {
+                r = m - 1;
+            } else if (m < x / m) {
+                l = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return r;
     }
 };
