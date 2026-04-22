4cdea368ed906c15f2ca4f4f4460fa8785709545
Kundan Kumar Sharma <146640526+kundan9199@users.noreply.github.com>
2026-04-22 20:31:29 +0530

    Time: 0 ms (100%), Space: 7.8 MB (52.06%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
+    //using sqrt()
+    //     bool isPerfectSquare(int num) {
+    //           if (num < 0) return false;
+
+    //     long long root = static_cast<long long>(sqrt(num));
+
+    //     return (root * root == num);
+    //     }
+    // };
+
+    //using binary search
     bool isPerfectSquare(int num) {
-          if (num < 0) return false; 
-    
-    long long root = static_cast<long long>(sqrt(num));
-    
-    return (root * root == num);
+        if (num < 1)
+            return false;
+        long left = 1, right = num;
+        while (left <= right) {
+            long mid = left + (right - left) / 2;
+            long square = mid * mid;
+            if (square == num)
+                return true;
+            if (square < num)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
     }
 };
