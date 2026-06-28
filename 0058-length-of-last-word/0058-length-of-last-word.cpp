b3ec77c369b5f49abcd05fbcc045b032201b5625
Kundan Kumar Sharma <146640526+kundan9199@users.noreply.github.com>
2026-06-29 02:04:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (34.16%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -3,7 +3,6 @@ public:
     int lengthOfLastWord(string s) {
         int n = s.length();
         int count = 0;
-
         for (int i = n - 1; i >= 0; i--) {
             if (s[i] != ' ') {
                 count++;
