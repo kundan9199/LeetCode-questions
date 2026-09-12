f34f62b139e6659d4e0e1a4a496c4f5c718738bf
Kundan Kumar Sharma <146640526+kundan9199@users.noreply.github.com>
2026-09-12 23:58:54 +0530

    Time: 4 ms (88.98%), Space: 19.1 MB (55.27%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -3,16 +3,13 @@ public:
     int lengthOfLongestSubstring(string s) {
     vector<int> lastSeen(256, -1);
     int lft = 0, maxLength = 0;
-
     for (int right = 0; right < s.length(); right++) {
         if (lastSeen[s[right]] >= lft) {
             lft = lastSeen[s[right]] + 1;
         }
-
         lastSeen[s[right]] = right;
         maxLength = max(maxLength, right - lft + 1);
     }
-
     return maxLength;
 }
 };
